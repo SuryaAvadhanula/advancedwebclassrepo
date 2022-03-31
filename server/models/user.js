7ae28a25f4a67682263e57f64c18fe00b0e2ca99
SuryaAvadhanula <88912055+SuryaAvadhanula@users.noreply.github.com>
2022-03-31 18:09:11 +0000

    Deploying to node-deploy from @ SuryaAvadhanula/advancedwebclassrepo@aba35e14148576659f5a8eaabea81cd493f5e49a 🚀

--- server/models/user.js
+++ server/models/user.js
@@ -1,36 +1,68 @@
+let hieghstId = 3;
+
 const list = [
     {
+        firstName: 'John',
+        lastName: 'Doe',
+        handle: 'johndoe',
+        password: 'password',
+        email: 'jhon@doe.com',
+        pic: 'https://randomuser.me/api/portraits/men/1.jpg',
         id: 1,
-        handle: 'Ryuk',
-        firstName: 'Lawliet',
-        lastName: 'Ryuzaki',
-        password: 'Deathnote',
-        email: 'l@gmail.com',
-        picture: 'https://randomuser.me/api/portraits/lego/4.jpg'
     },
     {
+        firstName: 'Vladimir',
+        lastName: 'Putin',
+        handle: 'russian_dictator',
+        password: 'long table',
+        email: 'jhon@doe.com',
+        pic: 'https://randomuser.me/api/portraits/men/2.jpg',
         id: 2,
-        handle: 'Kira',
-        firstName: 'Light',
-        lastName: 'Yagami',
-        password: 'Deathnote',
-        email: 'lyagami@gmail.com',
-        picture: 'https://randomuser.me/api/portraits/lego/5.jpg'
     },
     {
+        firstName: 'Kamala',
+        lastName: 'Harris',
+        handle: 'vp',
+        password: 'password',
+        email: 'kamala@whitehouse.org',
+        pic: 'https://randomuser.me/api/portraits/women/3.jpg',
         id: 3,
-        handle: 'One Piece',
-        firstName: 'Monkey',
-        lastName: 'Luffy',
-        password: 'Deathnote',
-        email: 'l@gmail.com',
-        picture: 'https://randomuser.me/api/portraits/lego/2.jpg'
-    }
+    },
 ];
 
 function get(id){
-    return list.find(user => user.id === parseInt(id));
+    return { ...list.find(user => user.id === parseInt(id)), password: undefined };
 }
 
-module.exports.list = list;
+function remove(id){
+    const index = list.findIndex(user => user.id === parseInt(id));
+    const user = list.splice(index,1);
+    
+    return { ...user[0], password: undefined};
+}
+
+function update(id, newUser){
+    const index = list.findIndex(user => user.id === parseInt(id));
+    const oldUser = list[index];
+
+    newUser = list[index] = { ...oldUser, ...newUser };
+
+    console.log(list);
+    
+    return { ...newUser, password: undefined};
+}
+
+module.exports = {
+    create(user) {
+        user.id = ++hieghstId;
+
+        list.push(user);
+        return { ...user, password: undefined};
+    },
+    remove,
+    update,
+    get list(){
+        return list.map(x=> ({...x, password: undefined }) );
+    }
+}
 module.exports.get = get;
