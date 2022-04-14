5ed6a06e12dd13855de2d65d6c0a512af99ffe94
SuryaAvadhanula <suryatejasharma@gmail.com>
2022-04-14 14:14:01 -0400

    updated controller and model

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express.Router();
+const { requireAuth } = require('../models/auth');
 
 const userModel = require('../models/user');
 
 const CREATED_STATUS = 201;
 
 app
-    .get('/', (req, res) => {
-        res.send(userModel.list);
+    .get('/', requireAuth, (req, res, next) => {
+        userModel.getList()
+        .then(users => {
+            res.send(users);
+        }).catch(next);
     })
-    .get('/:id', (req, res) => {
-
-        const user = userModel.get(req.params.id);
-        res.send(user);
-
+    .get('/handle/:handle', (req, res, next) => {
+        userModel.getByHandle(req.params.handle)
+        .then(user => {
+            res.send(user);
+        }).catch(next);
+    })
+    .get('/:id', (req, res, next) => {
+        userModel.get(req.params.id)
+        .then(user => {
+            res.send(user);
+        }).catch(next);
     })
     .post('/', (req, res, next) => {
         userModel.create(req.body)
@@ -21,11 +31,14 @@ app
             res.status(CREATED_STATUS).send(user);
         }).catch(next);
     })
-    .delete('/:id', (req, res) => {
+    .delete('/:id', requireAuth, (req, res, next) => {
 
-        const user = userModel.remove(req.params.id);
-
-        res.send({ success: true, errors: [], data: user });
+        userModel.remove(req.params.id)
+        .then(user => {
+            res.send({ success: true, errors: [], data: user });
+        }).catch(next);
+        //const user = userModel.remove(req.params.id);
+        //res.send({ success: true, errors: [], data: user });
 
     })
     .patch('/:id', (req, res, next) => {
@@ -35,6 +48,18 @@ app
             res.send({ success: true, errors: [], data: user });
         }).catch(next);
 
+    })
+    .post('/login', (req, res, next) => {
+        userModel.login(req.body.email, req.body.password)
+        .then(user => {
+            res.send(user);
+        }).catch(next);
+    })
+    .post('/seed', (req, res, next) => {
+        userModel.seed()
+        .then(x => {
+            res.send({ success: true, errors: [], data: x.insertedIds });
+        }).catch(next);
     })
 
 
